4fa71540ab928a1ea6f57b4b532bf2de2e926e85
kenneth <kenneth@KENNETHHPG4>
2016-10-25 04:29:57 +0800

    實作「客戶聯絡人」時，同一個客戶下的聯絡人，其 Email 不能重複。

---
+++ HomeWork1/Models/客戶資料.Partial.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HomeWork1.Models
+{
+    [MetadataType(typeof(客戶資料MetaData))]
+    public partial class 客戶資料MetaData
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public 客戶資料MetaData()
+        {
+            this.客戶銀行資訊 = new HashSet<客戶銀行資訊>();
+            this.客戶聯絡人 = new HashSet<客戶聯絡人>();
+        }
+
+        public int Id { get; set; }
+        [Required]
+        [StringLength(20, ErrorMessage = "{0} 的長度至少必須為 {2} ~ {1} 個字元。", MinimumLength = 3)]
+        public string 客戶名稱 { get; set; }
+        [Required]
+        [StringLength(8, ErrorMessage = "{0} 的長度必須為 {1} 個字元。", MinimumLength = 8)]
+        public string 統一編號 { get; set; }
+        [Required]
+        [StringLength(12, ErrorMessage = "{0} 的長度至少必須為 {2} ~ {1} 個字元。", MinimumLength = 8)]
+        public string 電話 { get; set; }
+        [Required]
+        [StringLength(12, ErrorMessage = "{0} 的長度至少必須為 {2} ~ {1} 個字元。", MinimumLength = 8)]
+        public string 傳真 { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "{0} 的長度至少必須為 {2} ~ {1} 個字元。", MinimumLength = 10)]
+        public string 地址 { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<客戶銀行資訊> 客戶銀行資訊 { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<客戶聯絡人> 客戶聯絡人 { get; set; }
+    }
+}
---
+++ HomeWork1/Models/客戶銀行資訊.Partial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HomeWork1.Models
+{
+    [MetadataType(typeof(客戶銀行資訊MetaData))]
+    public partial class 客戶銀行資訊MetaData
+    {
+        public int Id { get; set; }
+        public int 客戶Id { get; set; }
+        [Required]
+        [StringLength(20, ErrorMessage = "{0} 的長度至少必須為 {2} ~ {1} 個字元。", MinimumLength = 3)]
+        public string 銀行名稱 { get; set; }
+        [Required]
+        [Range(1000, 9000, ErrorMessage = "{0} 的值至少必須為 {1} ~ {2} 數字。")]
+        public int 銀行代碼 { get; set; }
+        [Required]
+        [Range(1000, 9000, ErrorMessage = "{0} 的值至少必須為 {1} ~ {2} 數字。")]
+        public Nullable<int> 分行代碼 { get; set; }
+        [Required]
+        [StringLength(20, ErrorMessage = "{0} 的長度至少必須為 {2} ~ {1} 個字元。", MinimumLength = 3)]
+        public string 帳戶名稱 { get; set; }
+        [Required]
+        [StringLength(20, ErrorMessage = "{0} 的長度至少必須為 {2} ~ {1} 個字元。", MinimumLength = 5)]
+        public string 帳戶號碼 { get; set; }
+        public virtual 客戶資料 客戶資料 { get; set; }
+    }
+}
